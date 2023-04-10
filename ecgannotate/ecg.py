e74b4e1ee2dac17161037f147f60e4d49a4b0299
Winston Zhang <wwzhang@umich.edu>
2023-04-09 22:14:38 -0400

    added functionality

--- ecgannotate/ecg.py
+++ ecgannotate/ecg.py
@@ -17,26 +17,13 @@ def findPeaks(fs, ecg):
     return r_peaks
     
 
-def calculateHR(r_peaks, fs, w_start, w_end):
+def calculateHR(num_peaks, fs, win_size):
     """Calculate the heart rate from R-peaks detected within the start and end of current window.
-
-    Args:
-        r_peaks (numpy array): An array of R-peak indices.
-        fs (float): The sampling frequency of the ECG signal.
-        w_start (int): Start of current window (index).
-        w_end (int): End of current window (index).
-
-    Returns:
-        float: Heart rate in current window.
-
     """
     # Calculate the duration of the window in seconds
-    window_duration = (w_end - w_start) / fs
-
-    # Count the number of R-peaks in the window using boolean indexing
-    r_peaks_in_window = np.count_nonzero((r_peaks >= w_start) & (r_peaks <= w_end))
+    window_duration = win_size / fs
 
     # Calculate the heart rate in bpm
-    heart_rate = r_peaks_in_window / window_duration * 60
+    heart_rate = num_peaks / window_duration * 60
 
     return heart_rate
--- ecgannotate/gui.py
+++ ecgannotate/gui.py
@@ -3,6 +3,7 @@ ECGannotate GUI main function
 """
 
 import numpy as np
+import csv
 
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
@@ -10,6 +11,9 @@ from pyqtgraph.console import ConsoleWidget
 from pyqtgraph.dockarea.Dock import Dock
 from pyqtgraph.dockarea.DockArea import DockArea
 from pyqtgraph.Qt import QtWidgets
+
+import ecg
+import sortMergeAnnotations
 
 
 class UI(QtWidgets.QWidget):
@@ -23,11 +27,15 @@ class UI(QtWidgets.QWidget):
         self.sr = 500 # default MIT-BIH sampling rate
         self.win_size = 10*self.sr  # 10 second default view
         self.idx = 0  # left-most plot time index
+        self.cur_win = 0
         
         # placeholder data
-        self.ecg = np.linspace(-1, 1, self.win_size*2)
+        self.ecg = np.linspace(-1, 1, self.win_size)
+        self.len = len(self.ecg)
+        self.peaks = np.array([])
+        self.max_win = 0
         # example table
-        # [{'start': 23, 'end': 48, 'type': 'AF'}]
+        # [{'win': 1, 'start': 23, 'end': 48, 'type': 'AF'}]
         self.table = []
         
         self.setupUI(self.win)
@@ -44,7 +52,7 @@ class UI(QtWidgets.QWidget):
         ## Note that size arguments are only a suggestion; docks will still have to
         ## fill the entire dock area and obey the limits of their internal widgets.
         d1 = Dock("File IO", size=(1000, 100))
-        d2 = Dock("Dock2", size=(200,400))
+        d2 = Dock("Plot Controls", size=(200,400))
         d3 = Dock("ECG Plot", size=(800,400))
         d4 = Dock("Annotations Table", size=(800,400))
 
@@ -66,19 +74,47 @@ class UI(QtWidgets.QWidget):
         loadBtn.clicked.connect(self.loadClicked)
         
         saveBtn = QtWidgets.QPushButton('Save annotations')
-        #restoreBtn.setEnabled(False)
+        saveBtn.clicked.connect(self.saveClicked)
         
         self.w1.addWidget(label, row=0, col=0)
         self.w1.addWidget(loadBtn, row=1, col=0)
         self.w1.addWidget(saveBtn, row=2, col=0)
         d1.addWidget(self.w1)
 
-        w2 = ConsoleWidget()
-        d2.addWidget(w2)
+        # plot controls and display
+        self.w2 = pg.LayoutWidget()
+        nextBtn = QtWidgets.QPushButton('Next window')
+        nextBtn.clicked.connect(self.nextClicked)
+        prevBtn = QtWidgets.QPushButton('Previous window')
+        prevBtn.clicked.connect(self.prevClicked)
+        self.winNum = QtWidgets.QLabel('Current window: 0 out of 0')
+        self.hrNum = QtWidgets.QLabel('Current window heart rate: 0')
+        self.pkNum = QtWidgets.QLabel('Number of R peaks detected: 0')
+        
+        self.boxLabel = QtWidgets.QLabel('Go to window: ')
+        self.winBox = QtWidgets.QSpinBox()
+        self.winBox.setRange(0, 0)
+        self.winBox.setSingleStep(1)
+        self.winBox.setValue(0)
+        self.winBox.valueChanged.connect(self.winBoxChanged)
+        
+        self.w2.addWidget(nextBtn, row=0, col=0)
+        self.w2.addWidget(prevBtn, row=1, col=0)
+        self.w2.addWidget(self.winNum, row=2, col=0)
+        self.w2.addWidget(self.boxLabel, row=3, col=0)
+        self.w2.addWidget(self.winBox, row=4, col=0)
+        self.w2.addWidget(self.pkNum, row=5, col=0)
+        self.w2.addWidget(self.hrNum, row=6, col=0)
+        
+        d2.addWidget(self.w2)
+        
 
         # w3 (ecg plot)
         self.w3 = pg.PlotWidget(title="ECG Viewer")
         self.w3_plot = self.w3.plot(self.ecg, pen=(255,255,255,200))
+        self.w3_scatter = pg.ScatterPlotItem(x=[], y=[], symbol='o', brush='r')
+        self.w3.addItem(self.w3_scatter)
+        
         self.w3.setMenuEnabled(False)
         d3.addWidget(self.w3)
         
@@ -87,25 +123,33 @@ class UI(QtWidgets.QWidget):
         self.w3.customContextMenuRequested.connect(self.plotRightMenu)
         
         self.action = QtGui.QAction(self)
-        self.action.setObjectName('af')        
-        self.action.setText ('AF')
+        self.action.setObjectName('af')
+        self.action.setText('AF')
 
         self.action1 = QtGui.QAction(self)
         self.action1.setObjectName('vt')
-        self.action1.setText ('VT')  
+        self.action1.setText('VT')
+        
+        self.action2 = QtGui.QAction(self)
+        self.action2.setObjectName('noise')
+        self.action2.setText('Noise')  
 
         self.customMenu = QtWidgets.QMenu('Menu', self.w3)       
         self.customMenu.addAction(self.action)
-        self.customMenu.addAction(self.action1)     
+        self.customMenu.addAction(self.action1)
+        self.customMenu.addAction(self.action2)
 
         self.action.triggered.connect(self.afClicked)
         self.action1.triggered.connect(self.vtClicked)
-        
+        self.action2.triggered.connect(self.noiseClicked)
         
         # w4 (annotation table)
         self.w4 = pg.TableWidget(editable=True)
         d4.addWidget(self.w4)
-        self.updateUI()
+        
+        self.updatePlot()
+        self.updateTable()
+    
     
     def loadClicked(self):
         '''ECG Load file button clicked'''
@@ -122,30 +166,104 @@ class UI(QtWidgets.QWidget):
             raw = np.load(fileName)
             if not np.isnan(raw).any():
                 self.ecg = raw.flatten()
+                self.len = len(self.ecg)
+                self.peaks = np.array(ecg.findPeaks(self.sr, self.ecg))
+                self.max_win = self.len // self.win_size
+                self.winBox.setRange(0, self.max_win-1)
             # clear preexisting annotations when loading new data
             self.table = []
-            self.updateUI()
+            self.updatePlot()
+            self.updateTable()
+    
+    def saveClicked(self, event):
+        if len(self.table) != 0:
+            # sort and merge annotations
+            new_table = sortMergeAnnotations.sortMergeAnnotations(self.table)
+            # Show a file dialog to choose where to save the CSV file
+            options = QtWidgets.QFileDialog.Options()
+            options |= QtWidgets.QFileDialog.DontUseNativeDialog
+            filename, _ = QtWidgets.QFileDialog.getSaveFileName(self.w1,
+                            "Save CSV File", "",
+                            "CSV Files (*.csv)",
+                            options=options)
+            if filename:
+                # Save the dictionary to the chosen CSV file
+                with open(filename, 'w', newline='') as csvfile:
+                    fieldnames = new_table[0].keys()
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    for row in new_table:
+                        writer.writerow(row)
+    
+    def nextClicked(self, event):
+        # plot next window
+        new_idx = self.idx + self.win_size
+        # check for out of bounds
+        if new_idx+self.win_size <= self.len:
+            self.idx = new_idx
+        self.updatePlot()
+    
+    def prevClicked(self, event):
+        # plot next window
+        new_idx = self.idx - self.win_size
+        # check for out of bounds
+        if new_idx >= 0:
+            self.idx = new_idx
+        self.updatePlot()
+    
+    def winBoxChanged(self, value):
+        self.idx = value * self.win_size
+        self.updatePlot()
     
     def plotRightMenu(self, event):
-        self.customMenu.popup (QtGui.QCursor.pos())
+        self.customMenu.popup(QtGui.QCursor.pos())
     
     def afClicked(self, event):
         '''Atrial fibrillation label for this time section'''
-        self.table.append({'start': self.idx, 
+        self.table.append({'win': self.cur_win,
+                           'start': self.idx, 
                            'end': self.idx+self.win_size, 
                            'type': 'AF'})
-        self.updateUI()
+        self.updateTable()
     
     def vtClicked(self, event):
         '''Ventricular Tachycardia label for this time section'''
-        self.table.append({'start': self.idx, 
+        self.table.append({'win': self.cur_win,
+                           'start': self.idx, 
                            'end': self.idx+self.win_size, 
                            'type': 'VT'})
-        self.updateUI()
+        self.updateTable()
     
-    def updateUI(self):
-        '''Refresh plots and table'''
-        self.w3_plot.setData(self.ecg[self.idx:self.idx+self.win_size])
+    def noiseClicked(self, event):
+        '''Noise label for this time section'''
+        self.table.append({'win': self.cur_win,
+                           'start': self.idx, 
+                           'end': self.idx+self.win_size, 
+                           'type': 'Noise'})
+        self.updateTable()
+    
+    def updatePlot(self):
+        '''Refresh plots and heart rate statistics'''
+        w_start = self.idx
+        w_end = self.idx+self.win_size
+        new_data = self.ecg[w_start:w_end]
+        self.cur_win = self.idx // self.win_size
+        
+        if self.peaks.size != 0:
+            self.cur_peaks = self.peaks[(self.peaks >= w_start) & (self.peaks <= w_end)]
+            new_hr = ecg.calculateHR(len(self.cur_peaks), self.sr, self.win_size)
+            # update statistics display
+            self.winNum.setText('Current window: ' + str(self.cur_win) + ' out of ' + str(self.max_win-1))
+            self.pkNum.setText('Number of R peaks detected: ' + str(len(self.cur_peaks)))
+            self.hrNum.setText('Current window heart rate: ' + str(new_hr))
+            if self.cur_peaks.size != 0:
+                plot_peaks = self.cur_peaks - self.idx
+                self.w3_scatter.setData(x=plot_peaks, y=new_data[plot_peaks])
+        
+        self.w3_plot.setData(new_data)
+    
+    def updateTable(self):
+        '''Refresh annotation table'''
         self.w4.setData(self.table)
 
 
